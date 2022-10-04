35537044e4251ba6544cbd948ee9029ac9cfd26b
dothihongkhanh <dothihongkhanh22@gmail.com>
2022-10-04 17:46:08 +0700

    design profile

--- TravelApplication/app/src/main/java/com/example/travelapplication/SignUpActivity.java
+++ TravelApplication/app/src/main/java/com/example/travelapplication/SignUpActivity.java
@@ -2,13 +2,38 @@ package com.example.travelapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+    EditText edtxtUNsignup;
+    EditText edtxtPWsignup;
+    EditText edtxtEmail;
+    EditText edtxtPhone;
+    Button btnBackSU;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        btnBackSU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent=new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+
+    public void AnhXa(){
+        edtxtUNsignup=(EditText) findViewById(R.id.editTextUserNameSU);
+        edtxtPWsignup=(EditText)  findViewById(R.id.editTextPassSU);
+        edtxtEmail=(EditText) findViewById(R.id.editTextEmail);
+        edtxtPhone=(EditText) findViewById(R.id.editTextSDT);
+        btnBackSU=(Button)  findViewById(R.id.buttonBackSU);
     }
 }
